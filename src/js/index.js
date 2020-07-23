ec74b76716080275a3ade278cf65a5de893b3259
Bryan De La Cruz <bdelacruza@pucp.edu.pe>
2020-07-23 00:33:39 -0500

    usando las 3 formas de importar mismo nombre,renombrando,todo(*)

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import str from './models/Search';
-import {add as f1,multiply as f2,ID} from './views/SearchView';
+// import {add as f1,multiply as f2,ID} from './views/SearchView';
+import * as SearchView from './views/SearchView';
 
-console.log(`Usando imported functions: ${f1(ID,2)} y ${f2(ID,2)} del view.
+console.log(`Usando imported functions: ${SearchView.add(SearchView.ID,2)} y ${SearchView.multiply(SearchView.ID,2)} del view.
 Del model => ${str}`);
+
